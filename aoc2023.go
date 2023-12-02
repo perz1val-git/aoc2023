8c49f85115ae6e31ed34de6de9a27d1aec6db93c
Perz1val <57048118+perz1val-git@users.noreply.github.com>
2023-12-02 01:21:25 +0100

    Day 1/1 done

--- aoc2023.go
+++ aoc2023.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	file, err := os.Open("inputs/day1_1.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+	sum := 0
+	for scanner.Scan() {
+		var first, last rune
+		for _, letter := range scanner.Text() {
+			if letter >= rune('0') && letter <= rune('9') {
+				if first == 0 {
+					first = letter
+					last = letter
+				} else {
+					last = letter
+				}
+			}
+		}
+		val, _ := strconv.Atoi(string(first) + string(last))
+		sum += val
+	}
+	fmt.Println(sum)
 }
