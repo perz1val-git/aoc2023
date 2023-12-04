109dcc581d1e323285d120dfc81e61bfb0acf113
Perz1val <57048118+perz1val-git@users.noreply.github.com>
2023-12-05 00:45:08 +0100

    Day 1 complete

--- aoc2023.go
+++ aoc2023.go
@@ -6,33 +6,56 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
-func day1_1() int {
-	file, err := os.Open("inputs/day1.txt")
+func getScanner(filename string) (*bufio.Scanner, *os.File) {
+	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
-	scanner := bufio.NewScanner(file)
+	return bufio.NewScanner(file), file
+}
+
+func day1() int {
+	scanner, file := getScanner("inputs/day1.txt")
 	sum := 0
 	for scanner.Scan() {
+		var firstPos, lastPos int = -1, -1
 		var first, last rune
-		for _, letter := range scanner.Text() {
-			if letter >= rune('0') && letter <= rune('9') {
-				if first == 0 {
-					first = letter
-					last = letter
-				} else {
-					last = letter
-				}
+		line := scanner.Text()
+
+		numberNames := []string{
+			"1", "2", "3",
+			"4", "5", "6",
+			"7", "8", "9",
+			"one", "two", "three",
+			"four", "five", "six",
+			"seven", "eight", "nine",
+		}
+		for number, numberName := range numberNames {
+			position := strings.Index(line, numberName)
+			if position < 0 {
+				continue
+			}
+			if position < firstPos || firstPos == -1 {
+				first = rune(number%9) + rune('1')
+				firstPos = position
+			}
+			lastPosition := strings.LastIndex(line, numberName)
+			if lastPosition > lastPos {
+				last = rune(number%9) + rune('1')
+				lastPos = lastPosition
 			}
 		}
+
 		val, _ := strconv.Atoi(string(first) + string(last))
 		sum += val
 	}
+	file.Close()
 	return sum
 }
 
 func main() {
-	fmt.Println(day1_1())
+	fmt.Println(day1())
 }
